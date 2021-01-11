2dff3fe0c8e6dd693a97246f681f96a668081b10
Alex Withington-Smith <alex@awithsmith.com>
2021-01-11 13:20:32 +0000

    private method to get formatted date, added transaction history

--- account.js
+++ account.js
@@ -1,12 +1,23 @@
 class Account {
   constructor() {
     this.balance = 0;
+    this.transactionHistory = [];
   }
 
   deposit(amount) {
     if (amount < 0) throw new Error("Deposits can only be positive!");
 
-    this.balance += this.#roundToTwoPlaces(amount);
+    let formattedAmount = this.#roundToTwoPlaces(amount)
+    this.balance += formattedAmount;
+
+    this.transactionHistory.push(
+        {
+            balance: this.balance,
+            type: 'credit',
+            amount: formattedAmount,
+            date: this.#getCurrentDate()
+        }
+    )
   }
 
   withdraw(amount) {
@@ -17,11 +28,35 @@ class Account {
   }
 
   getStatement() {
-      return 'No transaction history'
+      if (this.transactionHistory.length < 1) return 'No transaction history';
+
+      let statement = 'date || credit || debit || balance\n';
+      for (let transaction of this.transactionHistory) {
+          console.log(transaction);
+      }
+
+      return statement;
   }
 
   #roundToTwoPlaces(n) {
     return +n.toFixed(2);
+  }
+
+  #getCurrentDate() {
+      let today = new Date();
+      let day = today.getDate();
+      let month = today.getMonth() + 1;
+      let year = today.getFullYear();
+
+      if (day < 10) {
+          day = '0' + day;
+      }
+
+      if (month < 10) {
+          month = '0' + month;
+      }
+
+      return day + '/' + month + '/' + year;
   }
 }
 
