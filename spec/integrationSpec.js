8e9f06020b9d07eeb2baf90ef2a023db9b398501
Alex Withington-Smith <alex@awithsmith.com>
2021-01-12 09:08:10 +0000

    added more complex test case as outlined in the brief

--- spec/integrationSpec.js
+++ spec/integrationSpec.js
@@ -37,4 +37,19 @@ describe("Integrations", () => {
             "date || credit || debit || balance\n01/01/2020 || 15.00 || || 15.00"
         );
     });
+
+    it("Successfully formats the example given in the brief", () => {
+        jasmine.clock().mockDate(new Date(2012, 0, 10));
+        testAccount.deposit(1000)
+        jasmine.clock().mockDate(new Date(2012, 0, 13));
+        testAccount.deposit(2000)
+        jasmine.clock().mockDate(new Date(2012, 0, 14));
+        testAccount.withdraw(2000)
+        expect(testAccount.getStatement()).toEqual(
+            `date || credit || debit || balance
+            14/01/2012 || || 500.00 || 2500.00
+            13/01/2012 || 2000.00 || || 3000.00
+            10/01/2012 || 1000.00 || || 1000.00`
+        )
+    });
 });
